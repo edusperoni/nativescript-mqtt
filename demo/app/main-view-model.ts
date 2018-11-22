fb653f4df4cf2cc7aa62e9e1c554656dda746f10
Eduardo Speroni <edusperoni@gmail.com>
2018-11-22 17:49:41 -0200

    update demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,14 +1,45 @@
 import { Observable } from 'tns-core-modules/data/observable';
-import { MqttNew } from 'nativescript-mqtt-new';
+import { ClientOptions, MQTTClient } from 'nativescript-mqtt-new';
+import { Message } from 'nativescript-mqtt-new/common';
 
 export class HelloWorldModel extends Observable {
   public message: string;
-  private mqttNew: MqttNew;
+  private mqttClient: MQTTClient;
 
   constructor() {
     super();
 
-    this.mqttNew = new MqttNew();
-    this.message = this.mqttNew.message;
+    this.mqttClient = new MQTTClient({
+      clientId: "test",
+      host: "broker.mqttdashboard.com",
+      path: "/mqtt",
+      useSSL: false,
+      port: 8000
+    });
+
+    this.mqttClient.onConnectionSuccess.on(() => {
+      console.log("Mqtt connected");
+      this.set("message", "connected");
+      this.mqttClient.subscribe("testtopic/1");
+    });
+
+    this.mqttClient.onConnectionFailure.on((err) => {
+      console.log("Mqtt connection failure: " + err);
+      this.set("message", "Mqtt connection failure: " + err);
+    });
+
+    this.mqttClient.onConnectionLost.on((err) => {
+      console.log("Mqtt connection lost: " + err);
+      this.set("message", "Mqtt connection lost: " + err);
+    });
+
+    this.mqttClient.onMessageArrived.on((message: Message) => {
+      console.log(`Message received. Topic: ${message.topic}. Payload: ${message.payload}`);
+      this.set("message", `Message received. Topic: ${message.topic}. Payload: ${message.payload}`);
+    });
+
+    this.set("message", "connecting");
+
+    this.mqttClient.connect("", "");
   }
 }
