40b7dc8ba204bdaa059e70888e754b4df247522f
Eduardo Speroni <edusperoni@gmail.com>
2018-11-28 19:41:09 -0200

    proper testing

---
+++ demo/app/tests/tests.spec.ts
@@ -0,0 +1,84 @@
+import { MQTTClient, Message } from "nativescript-mqtt";
+
+function generateClient() {
+    return new MQTTClient({
+        clientId: "test",
+        host: "broker.mqttdashboard.com",
+        path: "/mqtt",
+        useSSL: false,
+        port: 8000
+    });
+}
+
+describe("connect function", function() {
+    let mqttClient: MQTTClient;
+
+    let connected = false;
+
+    function rebind() {
+        mqttClient.onConnectionFailure.off();
+        mqttClient.onConnectionSuccess.on(() => {
+            connected = true;
+            this.mqttClient.subscribe("testtopic/1");
+        });
+        mqttClient.onConnectionFailure.on((err) => {
+            connected = false;
+        });
+        mqttClient.onConnectionLost.on((err) => {
+            connected = false;
+        });
+        mqttClient.onMessageArrived.on((message: Message) => {
+        });
+    }
+
+    it("exists", function() {
+        expect(mqttClient.connect).toBeDefined();
+    });
+
+    beforeEach(() => {
+        mqttClient = generateClient();
+    });
+    afterEach(() => {
+        mqttClient.onConnectionSuccess.off();
+        mqttClient.onConnectionFailure.off();
+        mqttClient.onConnectionLost.off();
+        mqttClient.onMessageArrived.off();
+        mqttClient.disconnect();
+        mqttClient = null;
+    });
+
+    it("should connect", function(done) {
+        const success = () => {
+            done();
+        }
+        const fail = (e) => {
+            done.fail(e);
+        }
+        mqttClient.onConnectionSuccess.on(success);
+        mqttClient.onConnectionFailure.on(fail);
+        mqttClient.onConnectionLost.on(fail);
+        mqttClient.connect("", "");
+    }, 5000);
+
+    it("should not throw", function(done) {
+        let finished = false;
+        const attempts = 10;
+        const tout = 50;
+        for (let i = 0; i < attempts; i++) {
+            setTimeout(() => {
+                try {
+                    mqttClient.connect("", "");
+                } catch (e) {
+                    if (!finished)
+                        done.fail(e);
+                    finished = true;
+                }
+            }, i * tout);
+        }
+        setTimeout(() => {
+            if (!finished)
+                done();
+            finished = true;
+        }, attempts * tout);
+    }, 5000);
+});
